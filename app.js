77dfaa9f6440d6c531084904a3ca657caf2a21d9
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-01-21 12:10:41 -0500

    stage 2

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-const myHeading = document.getElementById('myHeading');
+const myList = document.getElementsByTagName('li');
 
-myHeading.addEventListener('click', () => {
-  myHeading.style.color = 'red';
+for (let i = 0; i < myList.length; i += 1) {
+  myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+  errorNotPurple[i].style.color = 'red';
+}
+
+const evens = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+  evens[i].style.backgroundColor = 'lightgray';
+}
+
+
+/*
+In the code above the js is running and turning everythign purple, then running and turning all classes that are not purple to red
+
+const myHeading = document.getElementsByTagName('h1')[0];
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+
+myButton.addEventListener('click', () => {
+  myHeading.style.color = myTextInput.value;
 });
+
+
+*/
